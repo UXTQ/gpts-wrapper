1bd3835efe6e630efb0e7b384f7aa11380be8041
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-02-12 22:01:31 +0000

    3.8.4

--- demo/index.ts
+++ demo/index.ts
@@ -1,3 +1,6 @@
 /*
 	demo:
-	install `ts-node` and run `ts-node index.ts` or built this fil
+	install `ts-node` and run `ts-node index.ts` or built this file and run js w node
+*/
+
+import { GpTs } from '../sr
