dcf552804a1542de9e8e0f6cadb594b4680f108b
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-02-23 10:48:26 +0000

    New version

--- demo/index.ts
+++ demo/index.ts
@@ -3,4 +3,6 @@
 	install `ts-node` and run `ts-node index.ts` or built this file and run js w node
 */
 
-import { GpTs } from '../sr
+import { GpTs } from '../src';
+
+import * as fs from 'fs'; // needs "@types/
