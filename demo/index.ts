d472ddfe8dcc1593b8195c0919c1501637d6ceb6
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-02-26 09:25:15 +0000

    Graphs

--- demo/index.ts
+++ demo/index.ts
@@ -9,3 +9,6 @@ import * as fs from 'fs'; // needs "@types/node": "^14.14.37",
 import * as path from 'path';
 
 (async () => {
+	const openai_apiKey = ''; // "sk-123..."
+	if (!openai_apiKey) {
+		co
