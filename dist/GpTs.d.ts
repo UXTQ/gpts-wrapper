f8184837af436238e3240f63fcb4bfe7423e998a
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-04-22 06:39:10 +0000

    cosmetic update

--- dist/GpTs.d.ts
+++ dist/GpTs.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="node" />
 import { AnswerRequest, AnswerResponse, ClassificationRequest, ClassificationResponse, CompletionRequest, CompletionResponse, EngineId, FileListResponse, EngineListResponse, EngineRetrieveResponse, SearchRequest, SearchResponse, FileUploadResponse, FileRetrieveResponse, EmbeddingsRequest } from './typings';
-import
+import * as fs from 'fs';
+import { EmbeddingsR
