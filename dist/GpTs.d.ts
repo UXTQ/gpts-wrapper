da3cc79df0edce6e602f7bb096d8877a9dcfa5cb
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-04-23 13:42:21 +0000

    add a note

--- dist/GpTs.d.ts
+++ dist/GpTs.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="node" />
 import { AnswerRequest, AnswerResponse, ClassificationRequest, ClassificationResponse, CompletionRequest, CompletionResponse, EngineId, FileListResponse, EngineListResponse, EngineRetrieveResponse, SearchRequest, SearchResponse, FileUploadResponse, FileRetrieveResponse, EmbeddingsRequest } from './typings';
 import * as fs from 'fs';
-import { EmbeddingsR
+import { EmbeddingsResponse } from '.';
+export declare class GpTs {
+    origi
