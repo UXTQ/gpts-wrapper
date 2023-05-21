6e2b1351ce89db0f66e1232bbccd79c0646bcb49
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-05-21 06:49:19 +0000

    Fix image link

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -3,4 +3,5 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["t
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+      
