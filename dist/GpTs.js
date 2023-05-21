477eeae242b94b444f5bfa40320dbe04b8f22946
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-05-21 09:01:24 +0000

    Removing useless code

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -4,4 +4,4 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-      
+        function step(result) { result.done ? resolve(result.value) :
