8052104e3a2fa4cc816c18ae07298536a0587f37
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-05-28 07:08:08 +0000

    update MANIFEST

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -5,4 +5,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments ||
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esMo
