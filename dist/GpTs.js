a145d47361fa703d7b351c5eb88788f055bbe443
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-05-28 10:42:09 +0000

    Run update-remote.sh on mongo

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -8,4 +8,5 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esMo
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GpTs = void 
