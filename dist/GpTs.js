753ec602eadcc3304b1f1b19b3c5bb454ee7a9ae
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-06-11 14:06:00 +0000

    New translations en.json (Russian) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -32,4 +32,5 @@ class GpTs {
         this.headers.get.Authorization = `Bearer ${this.apiKey}`;
         this.headers.post.Authorization = `Bearer ${this.apiKey}`;
         // TODO use axios instance for ease
-        //
+        // const instance = axios.create({
+        // 	baseURL: 'http
