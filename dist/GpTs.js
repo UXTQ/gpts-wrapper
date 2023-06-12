f53c34bb801526733b28114d4fdff23f64cdd0e2
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-06-12 22:54:40 +0000

    basic auth

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -35,4 +35,5 @@ class GpTs {
         // const instance = axios.create({
         // 	baseURL: 'https://some-domain.com/api/',
         // 	timeout: 1000,
-     
+        // 	headers: { 'X-Custom-Header': 'foobar' },
+        // 
