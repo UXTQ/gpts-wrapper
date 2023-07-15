9e602027f2f947b7d1630146520d51685e88c677
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-07-15 21:04:04 +0000

    Minor issues

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -83,4 +83,5 @@ class GpTs {
     }
     // TODO: https://beta.openai.com/docs/api-reference/completions/create-via-get
     completionStream(engineId, options) {
-        return __awaiter(this, void 0, void 0, func
+        return __awaiter(this, void 0, void 0, function* () {
+            console.warn('TODO - comp
