698aa98561964819a67c2973cdaf9cf54fad4a89
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-07-29 05:45:35 +0000

    update to index

--- dist/GpTs.js
+++ dist/GpTs.js
@@ -107,4 +107,4 @@ class GpTs {
     answer(options) {
         return __awaiter(this, void 0, void 0, function* () {
             const engineId = options.engineId;
-            dele
+            delete options.engineId; // some endpoints err if you pass in th
