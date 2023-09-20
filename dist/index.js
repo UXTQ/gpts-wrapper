14ac9f3ea7ccd9bb0d2bec34e599e631d1664485
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-09-20 19:32:54 +0000

    Update keybase.txt

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,6 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.
