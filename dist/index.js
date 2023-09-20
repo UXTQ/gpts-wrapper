efdca87e1fd7a7a1f1922d1c57b3c0d1da5dc9a3
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-09-20 15:27:47 +0000

    Hmm.

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,6 @@
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (functi
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    
