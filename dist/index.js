5e0812161d45d8b128b561c118f1e616715b58ee
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-09-21 10:29:46 +0000

    Modules

--- dist/index.js
+++ dist/index.js
@@ -9,4 +9,4 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-Ob
+Object.defineProperty(exports, "__esModule", { value: tru
