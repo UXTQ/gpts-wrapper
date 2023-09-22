2f24a23e21916e2c696832de6072abc363e99844
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-09-22 21:56:44 +0000

    updated template files

--- dist/typings.d.ts
+++ dist/typings.d.ts
@@ -73,3 +73,63 @@ export declare type SearchResponse = {
 };
 export declare type SearchResult = {
     document: number;
+    object: ObjectType.search_result;
+    score: number;
+};
+export declare type ClassificationRequest = BasicRequest & {
+    model?: EngineId;
+    query: string;
+    examples?: string[][];
+    file?: string;
+    labels?: string[];
+    search_model?: EngineId;
+    temperature?: number;
+    logprobs?: number;
+    max_examples?: number;
+    logit_bias?: any;
+    return_prompt?: boolean;
+    return_metadata?: boolean;
+    expand?: string[];
+};
+export declare type ClassificationResponse = {
+    completion: string;
+    label: string;
+    model: string;
+    object: ObjectType.classification;
+    search_model: EngineId;
+    selected_examples: {
+        document: number;
+        label: string;
+        text: string;
+    }[];
+};
+export declare type AnswerRequestBase = BasicRequest & {
+    model?: EngineId;
+    question: string;
+    examples: string[][];
+    examples_context: string;
+    search_model?: EngineId;
+    max_rerank?: number;
+    temperature?: number;
+    logprobs?: number;
+    max_tokens?: number;
+    stop?: string | string[];
+    n?: number;
+    logit_bias?: any;
+    return_metadata?: boolean;
+    return_prompt?: boolean;
+    expand?: string[];
+};
+export declare type AnswerRequestDocuments = AnswerRequestBase & {
+    documents: string[];
+};
+export declare type AnswerRequestFile = AnswerRequestBase & {
+    file: string;
+};
+export declare type AnswerRequest = AnswerRequestDocuments | AnswerRequestFile;
+export declare type AnswerResponse = {
+    answers: string[];
+    completion: string;
+    model: string;
+    object: ObjectType.answer;
+    search_model: EngineId;
