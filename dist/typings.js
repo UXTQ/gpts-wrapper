22ee1b5b7b343346ce1b1cb6b8ca484295cf9449
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-09-24 07:53:33 +0000

    removed some prints

--- dist/typings.js
+++ dist/typings.js
@@ -4,4 +4,5 @@ exports.ObjectType = void 0;
 var ObjectType;
 (function (ObjectType) {
     ObjectType[ObjectType["engine"] = 0] = "engine";
-    ObjectType[ObjectT
+    ObjectType[ObjectType["list"] = 1] = "list";
+    ObjectType[ObjectType["text
