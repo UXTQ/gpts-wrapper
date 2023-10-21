c16db015192654cdd958507d3aa54553d343740c
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-10-21 18:26:06 +0000

    2.9.1

--- src/GpTs.ts
+++ src/GpTs.ts
@@ -19,4 +19,5 @@ import {
 import axios from 'axios';
 
 // for file uploading
-import * as fs from 'fs'; // needs "@types/no
+import * as fs from 'fs'; // needs "@types/node": "^14.14.37",
+import * as FormData from 'form-data'
