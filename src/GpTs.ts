0faf3b4764fa83517f9473a105292a09d19820d7
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-10-21 23:01:16 +0000

    3.5.5

--- src/GpTs.ts
+++ src/GpTs.ts
@@ -20,4 +20,7 @@ import axios from 'axios';
 
 // for file uploading
 import * as fs from 'fs'; // needs "@types/node": "^14.14.37",
-import * as FormData from 'form-data'
+import * as FormData from 'form-data';
+import { EmbeddingsResponse } from '.';
+
+export class G
