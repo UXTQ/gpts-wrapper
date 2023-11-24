756efc19bf556b444bda6e76c698c9fa12bb9d21
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-11-24 20:06:28 +0000

    .ignore

--- src/GpTs.ts
+++ src/GpTs.ts
@@ -178,4 +178,5 @@ export class GpTs {
 
 	// "Only owners of organizations can delete files currently." (https://beta.openai.com/docs/api-reference/files/delete)
 	// not sure about the return type here as i am not an org owner
-	async fileDelete(fileId: 
+	async fileDelete(fileId: string): Promise<void> {
+		return await this.request<void>(`fil
